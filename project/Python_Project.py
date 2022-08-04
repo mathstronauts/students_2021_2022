96f1185485dedb4bf565a441db6a69379cad4a65
Daniel Wong <d.wong@mathstronauts.ca>
2022-08-04 14:27:52 -0400

    Added student template for python project

---
+++ project/Python_Project.py
@@ -0,0 +1,15 @@
+## Instructions/Goal/How to Play ##
+## The goal is to get the rocketship to land on mars
+## Click on the console to control (should be default)
+## Use the left and right arrow keys to aim the rocket
+## Press space to launch the rocket
+## The rocket cannot be controlled once it is launched
+
+## Imports
+
+mathstro.fullSetup()
+
+## Code for the game should go between the fullSetup and keySensingShutdown functions
+
+mathstro.keySensingShutdown()
+## print ending message here
